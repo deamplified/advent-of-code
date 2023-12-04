5ffc1d08aa18eb5c38d5281134ff840a3f66aa60
Sean Kelly <skelly215@gmail.com>
2023-12-04 09:53:12 -0500

    day 3 part 1

--- 2023/day3/index.js
+++ 2023/day3/index.js
@@ -1,29 +1,26 @@
 console.clear();
 import fs from "fs";
-// create 2d array/grid addressable by [v][h] coords
+
 const grid = fs
-  .readFileSync("./test-0.txt", "utf-8")
+  .readFileSync("./test-2.txt", "utf-8")
   .split("\n")
   .map((i) => i.split(""));
-// locs of all symbols
-const symbolLocs = (() => {
+
+const validSymbolLocs = (() => {
   const locs = [];
   for (let v = 0; v < grid.length; v++) {
-    // and each col h
     for (let h = 0; h < grid[v].length; h++) {
-      // char at current loc v/h
-      const data = grid[v][h];
-      // if char a symbol
-      if (data !== "." && isNaN(data)) {
-        // push loc to symbol list
+      const char = grid[v][h];
+
+      if (char !== "." && isNaN(char)) {
         locs.push([v, h]);
       }
     }
   }
   return locs;
 })();
-// locs of all chars w/in range of a symbol
-const validCharLocs = symbolLocs.flatMap((i) => {
+
+const validDigitLocs = validSymbolLocs.flatMap((i) => {
   const locs = [];
   const v = i[0];
   const h = i[1];
@@ -31,8 +28,8 @@ const validCharLocs = symbolLocs.flatMap((i) => {
     [-1, 0],
     [-1, 1],
     [0, 1],
-    [1, 0],
     [1, 1],
+    [1, 0],
     [1, -1],
     [0, -1],
     [-1, -1],
@@ -46,60 +43,34 @@ const validCharLocs = symbolLocs.flatMap((i) => {
   });
   return locs;
 });
-/*
 
-
-
-
-
-
-
-
-*/
-// list of all valid numbers from og text
-const validWholeNums = [];
-// given loc of symbol => locs of any digits within 1 char v/h
-function findWholeNums() {
-  // for every line of chars
+const validNums = (() => {
+  const nums = [];
   for (let v = 0; v < grid.length; v++) {
-    // start with an empty set
-    const numsInLine = [];
-    // check for numbers
-    const str = grid[v].join("").match(/\d+/);
-    // if any are found
-    if (str !== null) {
-      console.log("potential num found:", parseInt(...str)); // 142 (number)
-      // assume it's invalid
-      let vTotal = false;
-      // check every character location of the number
-      for (let h = str.index; h <= str[0].length; h++) {
-        // against every valid location
-        for (let i = 0; i < validCharLocs.length; i++) {
-          // if there's a match
-          if ([v, h].toString() === validCharLocs[i].toString()) {
-            // mark number  valid
-            vTotal = true;
+    let str = [...grid[v].join("").matchAll(/\d+/g)];
+    if (str.length > 0) {
+      for (let s = 0; s < str.length; s++) {
+        let isValid = false;
+        for (let h = str[s].index; h < str[s].index + str[s][0].length; h++) {
+          for (let i = 0; i < validDigitLocs.length; i++) {
+            if ([v, h].toString() === validDigitLocs[i].toString()) {
+              isValid = true;
+            }
           }
         }
-      }
-      // if any char locations were valid
-      if (vTotal === true) {
-        // add the number to the set
-        numsInLine.push(parseInt(...str));
-        console.log("valid num found:", parseInt(...str));
-        console.log("valid nums so ", numsInLine);
-        // and erase the chars
-        // for (let h = str.index; h <= str[0].length; h++) {
-        //   grid[v][h] = ".";
-        // }
+        if (isValid === true) {
+          nums.push(parseInt(str[s][0]));
+        }
       }
     }
   }
-}
-findWholeNums();
+  return nums;
+})();
+
+const sumOfNums = validNums.reduce((t, c) => t + c);
 /*
 
-
+`
 
 
 
@@ -110,8 +81,10 @@ findWholeNums();
 print();
 function print() {
   p(grid, "grid");
-  p(symbolLocs, "symbolLocs");
-  p(validCharLocs, "validCharLocs");
+  p(validSymbolLocs, "validSymbolLocs");
+  p(validDigitLocs, "validDigitLocs");
+  p(validNums, "validNums");
+  p(sumOfNums, "sumOfNums");
 
   function p(a, n) {
     console.log(`\n`);
